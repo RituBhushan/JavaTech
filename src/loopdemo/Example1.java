bbeef4a5811662eb261996f115f8fbf950909127
Samiksha Ghogare <samikshaghogare96@gmail.com>
2023-04-04 22:44:03 +0530

    Initial commit

---
+++ src/loopdemo/Example1.java
@@ -0,0 +1,16 @@
+package loopdemo;
+
+public class Example1 {
+	 
+	void display(String name, int count) {
+		for(int index=1;index<=count;index++)
+			System.out.println(name + "--" + index);
+		}
+	public static void main(String[] args) {
+		Example1 example1=new Example1();
+		example1.display(" SAMIKSHA ", 10);
+	}
+	}
+	
+
+ 
